2ea47da15142f0602e6cf958abd2654b6ddbd7b1
AnasAmil <anasamaneellah1@gmail.com>
2023-06-03 17:18:09 +0100

    started login form

---
+++ frontend/src/components/LoginForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {TextField} from '@mui/material';
+
+
+const LoginForm = () => {
+  return (
+    <form>
+        <TextField label="Email" variant="outlined"/>
+    </form>
+  )
+}
+
+export default LoginForm
