00a3e470e43192202b2b7e6f670b01b118fbfc7d
AnasAmil <anasamaneellah1@gmail.com>
2023-06-18 18:34:49 +0100

    reserve Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage'
 import {BrowserRouter, Routes,Route } from "react-router-dom"
 import useToken from './app/useToken';
+import ReservePage from './pages/ReservePage';
 
 function App() {
   const {token, setToken} = useToken()
@@ -16,6 +17,7 @@ function App() {
           <Route path='/' element={<HomePage />}/>
           <Route path='/history' element={<HistoryPage setToken={setToken} token={token} />}/>
           <Route path='/about' element={<AboutPage />}/>
+          <Route path='/reserve' element={<ReservePage />}/>
         </Routes>
       </BrowserRouter>
     </>
