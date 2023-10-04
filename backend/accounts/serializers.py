59f53a111cef699930172188f2311f6d675227ce
AnasAmil <anasamaneellah1@gmail.com>
2023-10-05 00:53:52 +0100

    finished auth

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -7,11 +7,11 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
-        def create(self, validated_data):
-            user = User(
-                email=validated_data['email'],
-                username=validated_data['username']
-            )
-            user.set_password(validated_data['password'])
-            user.save()
-            return user
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
